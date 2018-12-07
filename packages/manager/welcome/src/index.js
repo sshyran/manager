2cea29f05753227c81270fe876e6e9a931d0223f
Florian Renaut <florian.renaut@corp.ovh.com>
2018-12-07 16:36:19 +0100

    feat: play with React components

--- packages/manager/welcome/src/index.js
+++ packages/manager/welcome/src/index.js
@@ -1,7 +1,10 @@
 import angular from 'angular';
 import '@uirouter/angularjs';
 import ocLazyLoad from 'oclazyload';
+import react from 'react';
+import reactDom from 'react-dom';
 
+import './manager-welcome.less';
 import routing from './manager-welcome.routes';
 
 const moduleName = 'ovhManagerWelcome';
@@ -10,5 +13,47 @@ angular.module(moduleName, [
   ocLazyLoad,
   'ui.router',
 ]).config(routing);
+
+/**
+ * An attempt to wrap a React component inside AngularJS directive
+ */
+angular.module(moduleName).directive('reactComponent', /* @ngInject */ $timeout => ({
+  template: '<div></div>',
+  scope: {
+    component: '=',
+    bindings: '=',
+    props: '=',
+  },
+  link: function link(scope, element) {
+    const reactBindings = {};
+    Object.keys(scope.bindings || {}).forEach((key) => {
+      const callback = scope.bindings[key];
+      reactBindings[key] = (...args) => {
+        // synthetic React events needs to be persisted
+        args.forEach((a) => {
+          if (a.persist) {
+            a.persist();
+          }
+        });
+        // execute callback in AngularJS event loop
+        $timeout(() => {
+          callback(...args);
+        });
+      };
+    });
+
+    Object.keys(scope.props || {}).forEach((key) => {
+      reactBindings[key] = scope.props[key];
+    });
+
+    // unmount React when scope is destroyed
+    scope.$on('$destroy', () => {
+      reactDom.unmountComponentAtNode(element[0]);
+    });
+
+    // render React component
+    reactDom.render(react.createElement(scope.component, reactBindings, null), element[0]);
+  },
+}));
 
 export default moduleName;
--- packages/manager/welcome/src/manager-welcome.component.js
+++ packages/manager/welcome/src/manager-welcome.component.js
@@ -1,8 +1,38 @@
 import angular from 'angular';
+import buttons from 'react-svg-buttons';
+import Toggle from 'react-toggle';
+import Cards from 'react-credit-cards';
 import template from './manager-welcome.html';
 
 export default angular
   .module('OvhManagerWelcomeComponent', [])
   .component('ovhManagerWelcomeComponent', {
     template,
+    controller() {
+      this.foo = 0;
+      this.bar = false;
+      this.react = {
+        Button: buttons.PlusButton,
+        Toggle,
+        Cards,
+      };
+
+      this.creditCard = {
+        number: '1111111111111111',
+        name: 'foo',
+        expiry: '10/10',
+        cvc: 500,
+        focused: 'name',
+        issuer: 'visa',
+        preview: true,
+      };
+
+      this.reactButtonClicked = () => {
+        this.foo += 1;
+      };
+
+      this.reactToggleClicked = (e) => {
+        this.bar = e.target.checked;
+      };
+    },
   });
